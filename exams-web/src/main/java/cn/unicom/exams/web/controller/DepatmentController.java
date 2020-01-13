2db88145887fc2d8d5bd4050b842a45b831c1518
wangchanghe <576218934@qq.com>
2020-01-13 20:16:36 +0800

    实现User修改功能3

---
+++ exams-web/src/main/java/cn/unicom/exams/web/controller/DepatmentController.java
@@ -0,0 +1,37 @@
+package cn.unicom.exams.web.controller;
+
+import cn.unicom.exams.model.entity.SysDept;
+import cn.unicom.exams.service.service.ISysDeptService;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.Query;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author 王长何
+ * @create 2020-01-11 11:51
+ */
+@Controller
+@RequestMapping("/dept")
+public class DepatmentController {
+
+    @Resource
+    private ISysDeptService deptService;
+
+    @PostMapping("/deptnamelist")
+    @ResponseBody
+    public List<SysDept> deptnamelist(Integer delFlag){
+        QueryWrapper<SysDept> queryWrapper=new QueryWrapper<>();
+        queryWrapper.eq("del_flag",delFlag);
+        queryWrapper.orderByAsc("order_num");
+        return deptService.list(queryWrapper);
+    }
+}
